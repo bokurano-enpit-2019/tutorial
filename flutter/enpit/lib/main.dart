d769c7f91fda27bd58627286258a6f28567e5fe0
Mikihiro SUDA <su.glasses.923@gmail.com>
2019-04-25 10:44:06 +0900

    add: create flutter project

---
+++ flutter/enpit/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'where2eat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('where2eat'),
+      ),
+      body: Center(
+        child: Text('whew2eat'),
+      ),
+    );
+  }
+}
